e355580084f0272660a7fc23f47a5d0c252ba47a
Yiran Mao <yiranm@yiranm-macbookpro2.roam.corp.google.com>
2020-03-23 00:55:50 +0000

    Implemented the skeleton of global status and added corresponding style.

--- src/Screens/Map/index.js
+++ src/Screens/Map/index.js
@@ -4,12 +4,6 @@ import { MapComponent } from './MapComponent';
 
 export const Map = () => {
     return (
-<<<<<<< HEAD
     	<MapComponent />
-=======
-	<div className="container">
-    	<MapComponent />
-	</div>
->>>>>>> Further refactor to improve code quality and start styling the map component with bootstrap.
 	)
 }
