e9ff8b053bd864c083e80b9d111930e0ad10b95d
Hinesh Mandalia <hineshmandalia@gmail.com>
2020-03-29 14:56:37 +0100

    using className instead of class

--- src/Screens/Questionnaire/Steps/Additional.js
+++ src/Screens/Questionnaire/Steps/Additional.js
@@ -79,43 +79,43 @@ export const Additional = (props) => {
 
     return (
         <Step showNext={showSave()} onNext={() => saveReport()} step={props.step} nextText="Submit Self Report">
-            <div class="form-row">
-                <div class="form-group col-md-5">
+            <div className="form-row">
+                <div className="form-group col-md-5">
                     <TextInput label="First Name" value={firstName} setValue={setFirstName} />
                 </div>
-                <div class="form-group col-5 col-md-2">
+                <div className="form-group col-5 col-md-2">
                     <TextInput label="Middle Initial" value={middleName} setValue={setMiddleName} />
                 </div>
-                <div class="form-group col-md-5">
+                <div className="form-group col-md-5">
                     <TextInput label="Last Name" value={lastName} setValue={setLastName} />
                 </div>
             </div>
-            <div class="form-row">
-                <div class="form-group col-12 col-md-4">
+            <div className="form-row">
+                <div className="form-group col-12 col-md-4">
                     <label>Sex</label>
                     <CheckboxList question=" " options={options} type="radio" value={sex} setValue={setSex} />
                 </div>
-                <div class="form-group col-12 col-md-4">
+                <div className="form-group col-12 col-md-4">
                     <TextInput type="number" label="Age" value={age} setValue={setAge} />
                 </div>
             </div>
-            <div class="form-row">
-                <div class="col-12">
+            <div className="form-row">
+                <div className="col-12">
                     <LocationAutocomplete label="City of Residence" placeholder="City" value={city} setValue={setCity} />
                 </div>
             </div>
-            <div class="form-row mb-3">
-                <div class="col-12">
+            <div className="form-row mb-3">
+                <div className="col-12">
                     <TextInput type="email" label="Email Address" value={email} setValue={setEmail} />
                 </div>
             </div>
-            <div class="form-row">
-                <div class="col-12">
+            <div className="form-row">
+                <div className="col-12">
                     <Boolean question="Have you traveled outside your country of residence in the last 90 days?" value={travel} setValue={setTravel} />
                 </div>
             </div>
-            <div class="form-row">
-                <div class="col-12">
+            <div className="form-row">
+                <div className="col-12">
                     <Boolean question="Would you be interested in being updated about medical tests that can confirm whether you have had coronavirus and if you may be immune to the disease?" value={followUp} setValue={setFollowUp} />
                 </div>
             </div>
