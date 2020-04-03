c74355689f991566a4c0854f408d3012fb4feee8
Gary Devenay <garydevenay@Garys-MacBook-Pro.local>
2020-04-03 17:22:05 +0100

    Added navigation for about and privacy policy page. Also added the github widget to the top right

---
+++ src/Screens/Content/about.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export const About = () => {
+    return (
+        <div className="jumbotron">
+            <h1 className="display-4">About Us</h1>
+            <p>We’ve launched Operation COVID-19 (OpCovID) on a mission to identify all cases of the virus to prevent the spread, save lives and improve public health systems worldwide.</p>
+
+            <p>The idea came to our Founding Members in early March when one, Jillian, fell-ill with COVID-19 symptoms and could trace contact she made to the Prime Minister of Canada's wife Sophie Trudeau who tested positive. She and Gary, CTO of Safe & the City, began to formulate the idea of COVID-19 after being refused a test. Few are being tested, under-estimating the severity of the problem, limiting preventative measures and mobilisations into other public health monitoring systems needed. With your help to share your experiences, we can turn the table on this epidemic and build more intelligence to save lives.</p>
+
+            <p>We appreciate that most of us globally haven't been tested, which is why we've created a questionnaire collated from public health sources to help bridge this critical gap. A map to visualise to demonstrate how many official tests compared to suspected COVID-19 cases there are and work together with partners to provide more equitable testing to all.</p>
+
+            <p>Your privacy is of utmost importance. It doesn’t matter whether you’ve been tested for the virus, or not yet. Everyone’s experience counts. If we can contribute our experiences, we can inform each other, our health systems, and governments about the true picture of what’s going on in our communities.</p>
+
+            <p>Testing will be available shortly in the U.K. with other countries that have successfully implemented practices to flatten the curb and strain on our healthcare systems. By sharing our anonymous Covid stories, we’ll be able to prioritise these tests to those of us who need it first.</p>
+
+            <p>This is open-sourced, free, non-commercial driven project aimed to serve and protect everyone.</p>
+
+            <p>We teamed up with Gary Devenay, Jillian Kowalchuk, Paul Manning and Yiram Mao to create this open-source project with more getting involved every day.</p>
+
+            <p>If anyone wishes to volunteer skills or other support, please contact our team at <a href="mailto:operationcovid19@gmail.com">operationcovid19@gmail.com</a> or join us on our growing <a href="https://www.facebook.com/groups/1069877326719640/about/" target="_blank">Facebook Page</a>.</p>
+        </div>
+    )
+}
---
+++ src/Screens/Content/index.js
@@ -0,0 +1,7 @@
+import { PrivacyPolicy } from './privacy-policy';
+import { About } from './about';
+
+export {
+    PrivacyPolicy,
+    About
+}
